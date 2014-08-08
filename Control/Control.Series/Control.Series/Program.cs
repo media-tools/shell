eb83cfeb38acf90cd257dc61e50784b6ca6b31f0
Tobias Schulz <tobias.schulz186@gmail.com>
2014-08-08 11:50:50 +0200

    initial commit

---
+++ Control/Control.Series/Control.Series/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Control.Series
+{
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine ("Hello World!");
+        }
+    }
+}

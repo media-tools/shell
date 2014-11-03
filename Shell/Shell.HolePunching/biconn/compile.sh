99ffe99e7e8c6d682205476e60e006255ae33c16
Tobias Schulz <tobias.schulz186@gmail.com>
2014-11-03 03:11:45 +0100

    holepunching removed

--- Shell/Shell.HolePunching/biconn/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-g++ -o Shell.HolePunching/biconn/biconn -O3 Shell.HolePunching/biconn/biconn.cpp -Wall -pthread -std=c++11 -g
-i686-w64-mingw32-g++ -o Shell.HolePunching/biconn/biconn.exe -O3 Shell.HolePunching/biconn/biconn.cpp -Wall  -std=c++11 -lws2_32
-
